d5d95e6cae0c07b001669406539016183f71e91f
garima230306 <singh.garima809811@gmail.com>
2026-06-30 20:35:46 +0530

    second

---
+++ index3.js
@@ -0,0 +1,15 @@
+
+console.log("operator in javascript")   //operator in javascript
+let a = 45
+let b = 56
+console.log("a+b = ",a+b)
+console.log("a/b = ",a/b)
+console.log(a**b)
+console.log("a%b = " ,a%b)
+console.log("a-b = " ,a-b)
+console.log("a++ = " , a++)
+console.log(a++)
+console.log("a-- = " ,a--)
+console.log(a--)//
+console.log(++a)//
+console.log(a++)
